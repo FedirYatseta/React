17ff5c2b0144987df07e156177a51c021d327a15
Fedir <yatseta@gmail.com>
2021-01-23 11:52:15 +0200

    navlint exact

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogItem}>
-                <div className={s.dialog +' '+ s.active}>Fedir</div>
-                <div className={s.dialog}>Evgen</div>
-                <div className={s.dialog}>Ira</div>
-                <div className={s.dialog}>Kostia</div>
-                <div className={s.dialog}>Oleg</div>
-                <div className={s.dialog}>Andrey</div>
+                <div className={s.dialog +' '+ s.active}><NavLink to='/dialogs/1'>Fedir</NavLink></div>
+                <div className={s.dialog}><NavLink to='/dialogs/2'>Evgen</NavLink> </div>
+                <div className={s.dialog}><NavLink to='/dialogs/3'>Ira</NavLink></div>
+                <div className={s.dialog}><NavLink to='/dialogs/4'>Kostia</NavLink></div>
+                <div className={s.dialog}><NavLink to='/dialogs/5'>Oleg</NavLink></div>
+                <div className={s.dialog}><NavLink to='/dialogs/6'>Andrey</NavLink></div>
         </div>
             <div className={s.messages}>
                 <div className={s.message}>how are you?</div>
